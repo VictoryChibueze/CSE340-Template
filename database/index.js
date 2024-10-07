127b06367ed6b1f737e7c6498b6ae7aa26c31462
VictoryChibueze <alhajivicci12@gmail.com>
2024-10-07 19:29:23 +0100

    update

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
-const {Pool} = require("pg")
-require("dotenv").config()
+const { Pool } = require("pg");
+require("dotenv").config();
 /*  *************************
 
 *Connection Pool 
@@ -9,31 +9,33 @@ require("dotenv").config()
 
 * ***************************/
 
-
-let pool
-if(process.env.NODE_ENV === "development"){
-     pool = new Pool({
-        connectionString:process.env.DATABASE_URL,
-        ssl:{
-            rejectUnauthorized:false,
-        },
-    })
-
+let pool;
+if (process.env.NODE_ENV === "development") {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+  module.exports = pool;
 }
 
 // Added for troubleshooting queries
 // during development
 
 module.exports = {
-    async query(text,params){
-        try{
-            const res = await pool.query(text,params);
-            console.log("executed query",{text});
-            return res;
-        }catch(error){
-            console.error("error in query",{text})
-            throw error;
-            
-        }
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params);
+      console.log("executed query", { text });
+      return res;
+    } catch (error) {
+      console.error("error in query", { text });
+      throw error;
     }
-}
+  },
+};
