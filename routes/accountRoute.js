3522071a03c30c4d8eb0e0346f67f2ea26dc8a99
VictoryChibueze <alhajivicci12@gmail.com>
2024-10-15 16:09:39 +0100

    update

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,4 +5,8 @@ const acctController = require("../controllers/accountController");
 
 router.get("/login", utilities.handleErrors(acctController.buildLogin));
 router.get("/register", utilities.handleErrors(acctController.buildRegister));
+router.post(
+  "/register",
+  utilities.handleErrors(acctController.registerAccount)
+);
 module.exports = router;
