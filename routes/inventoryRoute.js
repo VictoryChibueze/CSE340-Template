5657e4b30673f91e33bf3282b937db57a7b87a2f
VictoryChibueze <alhajivicci12@gmail.com>
2024-10-20 06:14:52 +0100

    .

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
 const utilities = require("../utilities/");
+const invValidate = require("../utilities/inventory-validation");
 
 // Route to build inventory by classification view
 router.get(
@@ -13,6 +14,22 @@ router.get(
 router.get(
   "/detail/:invId",
   utilities.handleErrors(invController.buildInventoryDetails)
+);
+// add a new classification
+router.post(
+  "/add-classification",
+  utilities.checkAuthorized,
+  invValidate.classificationRules(),
+  invValidate.checkClassificationData,
+  utilities.handleErrors(invController.addClassification)
+);
+
+// add new inventory
+router.post(
+  "/add-inventory",
+  invValidate.inventoryRules(),
+  invValidate.checkNewInventoryData,
+  utilities.handleErrors(invController.addInventory)
 );
 
 module.exports = router;
